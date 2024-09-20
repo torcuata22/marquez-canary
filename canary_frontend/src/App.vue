0004ac32c2eb963f55c47c49925bce7952040baf
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2024-09-19 22:23:17 -0400

    setup complete -except postgres

---
+++ canary_frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <google-login @success="onLoginSuccess" @fail="onLoginFail" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onLoginSuccess(response) {
+      console.log('Google login successful:', response);
+      // You can send the response to your Django backend here via Axios
+    },
+    onLoginFail(error) {
+      console.log('Google login failed:', error);
+    }
+  }
+}
+</script>
---
+++ canary_frontend/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vue3GoogleLogin from 'vue3-google-login'
+
+const app = createApp(App)
+
+app.use(vue3GoogleLogin, {
+  clientId: 'YOUR_GOOGLE_CLIENT_ID' //need to request google client id
+})
+
+app.mount('#app')
